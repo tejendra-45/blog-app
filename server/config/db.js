114235ff3e925afbf2e3b5857cc5acfc772f1194
tejendra-45 <tejendrabylapudi@gmail.com>
2023-06-23 21:12:27 +0530

    blog

--- server/config/db.js
+++ server/config/db.js
@@ -3,5 +3,6 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/BlogApp").then(()=>{
     console.log("connected!");
 }).catch((err)=>{
+    console.log("ikkada unna");
     console.log("error");
 })
